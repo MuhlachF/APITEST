a4f78a956758f72563131b49f67bf4ae2e676379
Frederic MUHLACH <frederic.muhlach@gmail.com>
2023-09-19 10:31:25 +0200

    Ajout de la fonction d'authentification avec JWT

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
-const express = require('express')
-const basicAuth = require('express-basic-auth')
+const express = require('express');
 const app = express()
-const port = process.env.PORT || 3000
-app.use(express.json(),
-    basicAuth({
-        users: {
-            'john': 'superSecret',
-            'elise': '12345678',
-        },
-        challenge: true
-    })
-)
+const jwt = require('jsonwebtoken')
+const e_jwt = require('express-jwt');
+
+const SECRET = 'S3cr3tK3yD0ntSh4r31t'
+
+app.use(express.json())
+
+const users = [
+    {
+        "username": "john_doe",
+        "password": "123456",
+        "role": "USER"
+    },
+    {
+        "username": "jane_doe",
+        "password": "123456",
+        "role": "ADMIN"
+    }
+]
+
+app.use(e_jwt({ secret: SECRET, algorithms: ['HS256'] }).unless({ path: ['/login', '/'] }));
+
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send('Invalid token, access /login with username and password in the Body to authenticate.');
+    }
+}
+);
+
+app.post('/login', (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Error. Please enter the correct username and password' })
+    }
+
+    const user = users.find(u => u.username === req.body.username && u.password === req.body.password)
+    if (!user) {
+        return res.status(400).json({ message: 'Error. Wrong login or password' })
+    }
+
+    const token = jwt.sign({
+        username: user.username,
+        role: user.role
+    }, SECRET, { expiresIn: '3 hours' })
+
+    return res.json({ access_token: token })
+})
 
 // Une liste d'exemple de livres, qui pourrait être récupérée en base de données
 const books = [
